72e1491f1aa866b920e4a70cc24e77e18dce8f36
Repiboi <106424979+Repiboi@users.noreply.github.com>
2023-02-08 19:37:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//adding an alert function
+function fun(Name) {
+ 
+   alert ("NOTE: Once you sign in as a guest, No data is beign saved, So all data would be deleted after all.");
+ }
+ function wrong(){
+
+    alert("Incorrect directories");
+}
+
