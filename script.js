bc33a84fff444dcb72ae80361da186411c1ce031
Repiboi <106424979+Repiboi@users.noreply.github.com>
2023-02-21 16:13:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 function fun(Name) {
  
    alert ("NOTE: Once you sign in as a guest, No data is beign saved, So all data would be deleted after all.");
+   window.location.href="index.html"
  }
  function wrong(){
 
