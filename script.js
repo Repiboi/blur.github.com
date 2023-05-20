01fd88ffa9dc39a2387556e8a4a19f2a9f9388cf
Repiboi <106424979+Repiboi@users.noreply.github.com>
2023-05-20 17:55:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 //adding an alert function
 function fun(Name) {
- 
+
    alert ("NOTE: Once you sign in as a guest, No data is beign saved, So all data would be deleted after all.");
-   window.location.href="index.html"
+    window.location.href="index.html"
  }
  function wrong(){
 
     alert("Incorrect directories");
 }
+ document.getElementById("myButton").addEventListener("click",  function(){
+   window.location.href = "get.html";
+ });
+function get(){
+  window.location.href="get.html"
+}
+function next(){
+  window.location.href="login.html"
+}
+
+
+ function login(){
+      const user = document.getElementById("Username").value.toUpperCase();
+      const pass = document.getElementById("Password").value;
+  
+      if(pass === 123){
+        window.location.href="index.html"
+
+       }else{
+
+      alert("Incorect directories");
+ }
+
+ }
 
